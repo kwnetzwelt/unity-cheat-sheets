c098f12ce655f1579da41b49c68ea1be1c66bbad
Kai Wegner <kwnetzwelt@gmail.com>
2016-12-14 23:34:12 +0100

    initial commit

---
+++ New Unity Project/Assets/scripts/ConstRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstRotator : MonoBehaviour
+{
+    [SerializeField] private Vector3 axis;
+    [SerializeField] private float speed;
+
+	void Update () {
+	    this.transform.Rotate(axis,speed * Time.deltaTime);
+	}
+}
